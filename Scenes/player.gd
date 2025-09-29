691b1705d78f566c91e3ec840691bc34fb591201
Joona Niemenmaa <joonaoskari@hotmail.com>
2025-09-29 17:32:51 +0300

    tehtävä 1 tehty

---
+++ Scenes/player.gd
@@ -0,0 +1,18 @@
+class_name Player extends CharacterBody3D
+
+@export var MOVE_SPEED = 4.0
+@export var JUMP_HEIGHT = 5.0
+@export var GRAVITY = 9.8
+
+var y = 0
+var x = 0
+
+func _physics_process(delta: float) -> void:
+	x = Input.get_axis("left", "right") * MOVE_SPEED
+	
+	if (Input.is_action_just_pressed("jump") and is_on_floor()):
+		y = JUMP_HEIGHT
+	else:
+		y += get_gravity().y * delta
+	velocity = Vector3(x, y, 0)
+	move_and_slide()
